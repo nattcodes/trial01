80c80e15ebf8744e79c1f60769f0ebfb82755df1
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2022-07-28 06:33:43 +0100

    j

---
+++ src/Content.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Content = ({ colorValue, setColorValue}) => {
+    const inputColorValue = (colorValue) ? colorValue : 'Empty Value'
+    
+  return (
+    <div className='container'>
+        <h1 style={{backgroundColor : colorValue}}>
+            {inputColorValue}
+        </h1>
+        <form className='colorForm' onSubmit={(e) => e.preventDefault()}>
+            <input
+                type="text"
+                placeholder='Add color name'
+                required
+                value={colorValue}
+                onChange={(e) => setColorValue(e.target.value)}
+
+            />
+        </form>
+    </div>
+    
+  )
+}
+
+
+export default Content
+
