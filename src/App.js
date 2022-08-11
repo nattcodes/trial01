9246e1bf5515dc368d9c51d25349a2009d32017d
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2022-08-11 13:57:03 +0100

    d

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import { useState } from "react"
 import Content from "./Content"
 
 function App() {
-  const [colorValue, setColorValue] = useState('')
-  return (
-    <Content
-      colorValue={colorValue}
-      setColorValue={setColorValue}
-    />
-  )
+    const [colorValue, setColorValue] = useState('orange')
+    return ( <
+        Content colorValue = { colorValue }
+        setColorValue = { setColorValue }
+        />
+    )
 }
 
 export default App
