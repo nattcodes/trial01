8ef3ca83aa90ff2e3adc3ad2f194d6f427da5aa3
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2022-08-11 14:02:38 +0100

    j

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Content from "./Content"
 
 function App() {
-    const [colorValue, setColorValue] = useState('orange')
+    const [colorValue, setColorValue] = useState('')
     return ( <
         Content colorValue = { colorValue }
         setColorValue = { setColorValue }
